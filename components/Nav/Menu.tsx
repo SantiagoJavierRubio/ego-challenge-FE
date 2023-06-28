198139aa91311496ec7d850865295d10f844a56e
Santiago Javier Rubio <s.rubio91@gmail.com>
2023-06-28 11:32:54 -0300

    menu layout styling

--- components/Nav/Menu.tsx
+++ components/Nav/Menu.tsx
@@ -14,8 +14,10 @@ export const Menu: FC<MenuProps> = ({  }) => {
         setOpen(prev => !prev)
     }
   return (
-        <div className={`fixed top-0 right-0 p-2 pt-3 ${open ? 'flex-col bottom-0 min-h-screen h-full w-1/5 bg-white' : 'flex-row bottom-full'}`}>
-            <div className='flex gap-2 items-center justify-end'>
+    <>
+        <div className={`fixed top-0 right-0 bottom-0 min-h-screen h-full bg-white ${open ? 'w-1/5' : 'w-0'} transition-all`} id="menu-background"/>
+        <div className={`fixed top-0 right-0 ${open ? 'flex-col bottom-0 min-h-screen h-full w-1/5' : 'flex-row bottom-full'}`}>
+            <div className='flex gap-2 pr-4 items-center justify-end translate-y-4'>
                 <p>
                     {open ? 'Cerrar' : 'Menu'} 
                 </p>
@@ -23,26 +25,48 @@ export const Menu: FC<MenuProps> = ({  }) => {
                     {open ? <AiOutlineClose size={28} /> : <HiOutlineMenu  size={28} />}
                 </button>
             </div>
-            {open &&
-                <div className='flex-col gap-4 text-right mt-8 pr-8'>
-                    <div className='flex-col gap-2'>
-                        <p><a href="/modelos">Modelos</a></p>
-                        <p><a href="/servicios">Servicios y Accesorios</a></p>
-                        <p><a href="/financiacion">Financiación</a></p>
-                        <p><a href="/reviews">Reviews y Comunidad</a></p>
-                    </div>
-                    <div className='flex-col gap-2'>
-                        <p><a href="/mobility">Toyota Mobility Service</a></p>
-                        <p><a href="/gazoo">Toyota Gazoo Racing</a></p>
-                        <p><a href="/hibridos">Toyota Híbridos</a></p>
-                    </div>
-                    <div className='flex-col gap-2'>
-                        <p><a href="/concesionarios">Concesionarios</a></p>
-                        <p><a href="/testdrive">Test Drive</a></p>
-                        <p><a href="/contacto">Contacto</a></p>
-                    </div>
+            <div className={`flex flex-col text-right h-full justify-evenly mt-6 origin-right ${open ? 'scale-x-100 opacity-100' : 'scale-x-0 opacity-0'} transition-opacity delay-100 ease-in-out`}>
+                <div className='flex-col flex pr-8 justify-start'>
+                    <MenuLink url="/modelos" text="Modelos" />
+                    <MenuLink url="/servicios" text="Servicios y Accesorios" />
+                    <MenuLink url="/financiacion" text="Financiación" />
+                    <MenuLink url="/reviews" text="Reviews y Comunidad" />
                 </div>
-            }
+                <hr className='self-center w-5/6' />
+                <div className='flex-col flex pr-8 justify-start'>
+                    <MenuLink url="/mobility" text="Toyota Mobility Service" />
+                    <MenuLink url="/gazoo" text="Toyota Gazoo Racing" />
+                    <MenuLink url="/hibridos" text="Toyota Híbridos" />
+                </div>
+                <hr className='self-center w-5/6' />
+                <div className='flex-col flex pr-8 justify-start'>
+                    <MenuLink url="/concesionarios" text="Concesionarios" />
+                    <MenuLink url="/testdrive" text="Test Drive" />
+                    <MenuLink url="/contacto" text="Contacto" />
+                </div>
+                <div className='bg-gray-300/75 pr-8 flex flex-col justify-start'>
+                    <MenuLink url="/actividades" text="Actividades" />
+                    <MenuLink url="/atencion-clientes" text="Servicios al Cliente" />
+                    <MenuLink url="/ventas-especiales" text="Ventas Especiales" />
+                    <MenuLink url="/innovacion" text="Innovación" />
+                    <MenuLink url="/prensa" text="Prensa" />
+                    <MenuLink url="/about" text="Acerca de..." />
+                </div>
+            </div>
         </div>
+        </>
   )
+}
+
+interface MenuLinkProps {
+    text: string,
+    url: string
+}
+
+const MenuLink: FC<MenuLinkProps> = ({ text, url }) => {
+    return (
+        <p className='my-2'>
+            <a href={url}>{text}</a>
+        </p>
+    )
 }
--- components/Nav/NavBar.tsx
+++ components/Nav/NavBar.tsx
@@ -7,7 +7,7 @@ interface NavBarProps {
 
 export const NavBar: FC<NavBarProps> = ({  }) => {
   return (
-    <nav className='fixed top-0 left-0 right-0 w-full flex justify-start gap-8 p-2'>
+    <nav className='fixed top-0 left-0 right-0 w-full flex justify-start gap-8 p-2 bg-white'>
         <img src="/ego-logo.svg" width="38px" alt="Logo de la compania" />
         <div className='flex items-center gap-4'>
             <div>Modelos</div>
